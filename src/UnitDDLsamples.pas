0223df75abbf9a49df0f21e261174b9305763b8f
jackyxinli <jackyxinli@163.com>
2019-01-25 17:06:04 +0800

    Create UnitDDLsamples.pas

---
+++ src/UnitDDLsamples.pas
@@ -0,0 +1,25 @@
+unit UnitDDLsamples;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TfrmDDLsamples = class(TForm)
+    Memo1: TMemo;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmDDLsamples: TfrmDDLsamples;
+
+implementation
+
+{$R *.dfm}
+
+end.
